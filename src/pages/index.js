4dc46cabf288b505a42a58efda52a2d894c56d3d
BJuice <beetle_juice84@mail.ru>
2023-02-21 20:12:29 +0300

    fix/Исправить синтаксические ошибки и помарки в data.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import { TicTacToe } from "../components/TicTacToe.js";
 
 const game = document.getElementById('game');
 const result = document.getElementById('result');
-const resetBtn = document.getElementById('reset');
+const startBtn = document.getElementById('reset');
 const boardSize = 9;
 const template = document.querySelector('.template').content;
 
 const ticTacToe = new TicTacToe(game, result, boardSize, template);
 
 ticTacToe.startGame();
-resetBtn.addEventListener("click", () => ticTacToe.startGame());
+startBtn.addEventListener("click", () => ticTacToe.startGame());
